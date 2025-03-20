345d55d07046d352e93aa05dd00f6448dc16db8a
digitribesthlm <patrik@makeablesthlm.se>
2025-03-20 18:30:01 +0100

    Next

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link href="https://cdn.jsdelivr.net/npm/@n8n/chat/dist/style.css" rel="stylesheet" />
+        <style>
+          .chat-window-toggle {
+            background-color: #f36b23 !important;
+          }
+        </style>
       </Head>
       <body>
         <Main />
